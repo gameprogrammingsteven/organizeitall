6efec0ec2e7848f5b411a8d765d39c9fa54e0a41
moyarich <moyarich@hotmail.com>
2020-04-21 20:28:39 -0500

    Initial file upload
     final project submitted

---
+++ OrganizeItAll/model/data/List+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  List+CoreDataClass.swift
+//  OrganizeItAll
+//
+//  Created by MOYA RICHARDS on 3/15/20.
+//  Copyright © 2020 MOYA RICHARDS. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(List)
+public class List: NSManagedObject {
+
+}
---
+++ OrganizeItAll/model/data/Task+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Task+CoreDataClass.swift
+//  OrganizeItAll
+//
+//  Created by MOYA RICHARDS on 3/14/20.
+//  Copyright © 2020 MOYA RICHARDS. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
