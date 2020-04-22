a33550a4a4d791258b5083e5050b5f1e6d3a9d08
StephenJohnson <sjjair@yahoo.com>
2020-04-21 21:14:04 -0700

    Slight reorganizing for universal objects

---
+++ OrganizeItAll/Core Engine/UI/Circle.swift
@@ -0,0 +1,21 @@
+//
+//  Circle.swift
+//  OrganizeItAll
+//
+//  Created by Stephen Johnson on 4/21/20.
+//  Copyright © 2020 MOYA RICHARDS. All rights reserved.
+//
+
+import UIKit
+
+class Circle: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
